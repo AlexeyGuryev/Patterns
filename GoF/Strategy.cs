96b93a95a8aefe6f03923855adb8ad269c24f8a9
Alexey Guryev <alguryew@gmail.com>
2015-10-17 21:32:57 +0300

    Strategy pattern

--- GoF/Strategy.cs
+++ GoF/Strategy.cs
@@ -10,11 +10,74 @@ namespace GoF
     {
         public void Run()
         {
-            //throw new NotImplementedException();
+            var firstExample = new CalcData
+            {
+                A = 3,
+                B = 2
+            };
+
+            var calculator = new Calculator(firstExample);
+            calculator.CalcStrategy = new StrategyAddition();
+
+            Console.WriteLine("{0} with {1} = {2}", firstExample.A, firstExample.B, calculator.Calc());
+
+            calculator.CalcStrategy = new StrategySubtraction();
+            Console.WriteLine("{0} with {1} = {2}", firstExample.A, firstExample.B, calculator.Calc());
+
+            Console.WriteLine("{0} with {1} = {2}", firstExample.A, firstExample.B, calculator.Calc(c => c.A * c.B));
         }
     }
 
-    public class Strategy
+    public interface ICalulationStrategy
     {
+        int CalcTwoNumbers(int a, int b);
+    }
+
+    public class Calculator
+    {
+        private CalcData calcData;
+        public ICalulationStrategy CalcStrategy { get; set; }
+
+        public Calculator(CalcData calcData)
+        {
+            this.calcData = calcData;
+        }
+
+        public int Calc()
+        {
+            if (CalcStrategy == null)
+            {
+                throw new ArgumentNullException("CalcStrategy need to be set firstly!");
+            }
+
+            return CalcStrategy.CalcTwoNumbers(calcData.A, calcData.B);
+        }
+
+        public int Calc(Func<CalcData, int> twoNumbersCalculator)
+        {
+            return twoNumbersCalculator(calcData);
+        }
+    }
+
+    public class CalcData
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+    }
+
+    public class StrategyAddition : ICalulationStrategy
+    {
+        public int CalcTwoNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    public class StrategySubtraction : ICalulationStrategy
+    {
+        public int CalcTwoNumbers(int a, int b)
+        {
+            return a - b;
+        }
     }
 }
